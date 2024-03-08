8105b66b6778beb10572be1b18e1231009fc3880
DesireeEspinosa <desireeespinosacontreras@gmail.com>
2024-03-08 15:43:44 -0600

    Commented out Model3D route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PilotContainer from "./Components/PilotContainer/PilotContainer";
 import PilotPage from './Pages/Pilot/Pilot';
-import Model3D from './Pages/Model3D/Model3D';
+// import Model3D from './Pages/Model3D/Model3D';
 
 function scale(number, inMin, inMax, outMin, outMax) {
   return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
@@ -18,7 +18,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<PilotPage/>} />
-          <Route path="/model/:diameter/:height" element={<Model3D/>} />
         </Routes>
       </Router>
       </>
