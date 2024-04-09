93156cffad6694499ef35b751f9e8090c8c29f24
Oscar Zepeda <iwemaird@gmail.com>
2024-04-09 13:52:50 -0600

    Added graph component styles and functionality

--- src/Components/GraphProp/Graph.js
+++ src/Components/GraphProp/Graph.js
@@ -4,12 +4,41 @@ import "./Graph.css";
 
 export default function Graph() {
     const [imageSrc, setImageSrc] = useState(null);
+    const [file, setFile] = useState(null);
+    const [message, setMessage] = useState("");
 
     const handleFileUpload = async (evt) => {
-        const file = evt.target.files[0];
-        if (!file) return;
-    }
+        const uploadedFile = evt.target.files[0];
+        if (!uploadedFile) return;
+    
+        if (uploadedFile.type === "text/csv") {
+          setFile(uploadedFile);
+        } else {
+          setMessage("Please upload a CSV file.");
+        }
+    };    
 
+    const handleUpload = async () => {
+        if (file) {
+          const formData = new FormData();
+          formData.append("file", file);
+    
+          try {
+            const response = await fetch("http://127.0.0.1:5000/upload", {
+              method: "POST",
+              body: formData,
+            });
+    
+            if (response.ok) {
+              setImageSrc("http://127.0.0.1:5000/image/temp_plot.png");
+            }
+          } catch (error) {
+            setMessage("Fatal error :(");
+          }
+        } else {
+          setMessage("Please select a file first.");
+        }
+      };
 
     return (
         <div className="GraphContainer">
@@ -23,8 +52,17 @@ export default function Graph() {
                     style={{ display: "none" }}
                 />
             </label>
+            <label className="upload-button-label">
+                <span className="upload-text">Generate</span>
+                <button
+                    id="file-input"
+                    className="file-upload"
+                    onClick={handleUpload}
+                    style={{ display: "none" }}
+                />
+            </label>
             {imageSrc && (
-                <div className="chart-image-container">className
+                <div className="chart-image-container">
                     <img id="chart-image" className="chart-image" src={imageSrc} alt="chart" />
                 </div>
             )}
